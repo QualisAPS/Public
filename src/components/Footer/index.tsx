8a1be2eb4b9360378c6a0d4662e417471334f97a
gabrielbsb21 <gabrielbsb21@outlook.com>
2023-06-07 22:48:28 -0300

    working on footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
     <footer className="mt-11 bg-green-variant">
-      <p>footer</p>
+      <div className="mx-auto max-w-7xl px-4 py-2 sm:px-6 lg:px-8">
+        <p>footer</p>
+      </div>
     </footer>
   );
 }
